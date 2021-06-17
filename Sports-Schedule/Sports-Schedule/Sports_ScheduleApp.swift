c17c3df2785a88cc217b03e096955fa0ee7cd78b
James Wiryo <jamesgiggs95@gmail.com>
2021-06-17 17:34:25 +0800

    Initialise a new iOS App Project

---
+++ Sports-Schedule/Sports-Schedule/Sports_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sports_ScheduleApp.swift
+//  Sports-Schedule
+//
+//  Created by James Denny Wiryo on 17/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Sports_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
